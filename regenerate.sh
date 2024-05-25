33863a541d6e356df2251fb6e04b3635be127f70
Dávid Horváth <horvath@webarticum.hu>
2024-05-25 07:26:51 +0200

    Add a minimal content

---
+++ regenerate.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#----------
+# Regenerates the site based on some master content.
+#----------
+
+
+rootDir="$( dirname -- "$( realpath "$0" )" )"
+
+regenerateIcon () {
+    iconDir="$1"
+    rm -f "${iconDir}/64x64.png" "${iconDir}/32x32.png" "${iconDir}/favicon.ico"
+    convert "${iconDir}/128x128.png" -resize 64x64 "${iconDir}/64x64.png"
+    convert "${iconDir}/128x128.png" -resize 32x32 "${iconDir}/32x32.png"
+    convert "${iconDir}/128x128.png" "${iconDir}/64x64.png" "${iconDir}/32x32.png" "${iconDir}/favicon.ico"
+} 
+
+regenerateIcon "${rootDir}/icon"
+regenerateIcon "${rootDir}/holodb/icon"
